952d167aabbdcf239ea543eaefc0f08e0d4d387d
Nadine <nadine.pigida@live.com>
2020-06-21 14:46:24 -0400

    Sleep Schedule
    Added time thingy to sleep thingy

--- app/src/main/java/com/defhacks/forgetmenot/MainActivity.java
+++ app/src/main/java/com/defhacks/forgetmenot/MainActivity.java
@@ -3,9 +3,11 @@ package com.defhacks.forgetmenot;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
 
--- app/src/main/java/com/defhacks/forgetmenot/SleepActivity.java
+++ app/src/main/java/com/defhacks/forgetmenot/SleepActivity.java
@@ -2,13 +2,46 @@ package com.defhacks.forgetmenot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class SleepActivity extends AppCompatActivity {
+
+    TextView mTimeTextView;
+    Button mPickTimeButton;
+    Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_schedule);
+
+        mTimeTextView = (TextView) findViewById(R.id.time_text_view);
+        mPickTimeButton = (Button) findViewById(R.id.pick_time_button);
+
+        Calendar calendar = Calendar.getInstance();
+
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+
+        mPickTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(mContext, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        mTimeTextView.setText(hourOfDay + ":" + minute);
+                    }
+                },hour,minute,android.text.format.DateFormat.is24HourFormat(mContext));
+                timePickerDialog.show();
+            }
+        });
     }
 }
